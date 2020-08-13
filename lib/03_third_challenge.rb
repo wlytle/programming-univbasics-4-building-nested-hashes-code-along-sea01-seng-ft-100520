678717214d1d9b599e8d693a80a7b361ed111c37
Will Lytle <wlytle89@gmail.com>
2020-08-13 20:47:50 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,41 +2,58 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => ""
-        :age => ""
+        :name => "Lord Montague"
+        :age => "53"
         },
       :matriarch => {
-        :name => ""
-        :age => ""
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
-        :name => ""
-        :age => ""
+        :name => "Romeo"
+        :age => "15"
         },
-      :hero_friends => [
-        :name => ""
-        :age => ""
-        :attitude => ""
-        ]
+      :hero_friends => 
+      [
+        {
+        :name => "Benvolio"
+        :age => "17"
+        :attitude => "worried"
+        
+      },
+      {
+        :name => "Mercutio"
+        :age => "18"
+        :attitude => "hot-headed"
+      }
+    ]
    }, 
    :capulet => {
       :patriarch => {
-        :name => ""
-        :age => ""
+        :name => "Lord Capulet"
+        :age => "50"
         },
       :matriarch => {
-        :name => ""
-        :age => ""
+        :name => "Lady Capulet"
+        :age => "51"
         },
       :heroine => {
-        :name => ""
-        :age => ""
+        :name => "Juliet"
+        :age => "15"
         },
-      :heroine_friends => [
-        :name => ""
-        :age => ""
-        attitude => ""
-        ]
+      :heroine_friends => 
+      [
+        {
+          :name => "Steven"
+          :age => "30"
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse"
+          :age => "44"
+          :attitude => "worried"
+        }
+      ]
    }
   }
 
