8d9c50d5aeb77be7b047a1b8543e5f9174b83b75
Will Lytle <wlytle89@gmail.com>
2020-08-13 20:51:16 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -9,6 +9,7 @@ def third_challenge
       :hero => {
         :name => "Romeo",
         :age => "15",
+        :status => "alive"
         },
       :hero_friends => 
       [
@@ -36,7 +37,8 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => 
       [
