14b27261ab2dea16cec8ea5533369b3c853e89f2
Will Lytle <wlytle89@gmail.com>
2020-08-13 20:52:38 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,8 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         { :name => "Benvolio", :age => "17", :attitude => "worried" },
-      { :name => "Mercutio", :age => "18", :attitude => "hot-headed" }
-        #your hashes here!
+        { :name => "Mercutio", :age => "18", :attitude => "hot-headed" }
       ]
    }, 
    :capulet => {
